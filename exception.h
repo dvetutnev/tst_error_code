55bc49709968bef6295944e5f30375147b95ab91
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-01-16 19:42:32 +0300

    Use template for ctor exceptions

--- exception.h
+++ exception.h
@@ -10,9 +10,17 @@ namespace net
 class Exception : public std::system_error
 {
 public:
-    template<typename... Args>
-    Exception(Args... args)
-        : system_error{args...}
+    template<typename Errc,
+             typename = std::enable_if< std::is_enum<Errc>::value >::type>
+    Exception(Errc ec)
+        : system_error{ec}
+    {}
+
+    template<typename Errc, typename String,
+             typename = std::enable_if< std::is_enum<Errc>::value >::type,
+             typename = std::enable_if< std::is_convertible<String, std::string>::value >::type>
+    Exception(Errc ec, String what_str)
+        : system_error{ec, what_str}
     {}
 };
 
@@ -21,6 +29,12 @@ class InvalidContext : public Exception
 public:
     InvalidContext()
         : Exception{AdapterErrors::InvalidContext}
+    {}
+
+    template<typename String,
+             typename = std::enable_if< std::is_convertible<String, std::string>::value >::type>
+    InvalidContext(String what_str)
+        : Exception{AdapterErrors::InvalidContext, what_str}
     {}
 };
 
--- test_adapter_errors.cpp
+++ test_adapter_errors.cpp
@@ -27,3 +27,25 @@ TEST(AdapterErrors, exception) {
     }
     ASSERT_TRUE(true);
 }
+
+TEST(AdapterErrors, exception_with_str) {
+    try {
+        throw net::InvalidContext{"Additional string"};
+        //throw net::InvalidContext{42};
+        //throw net::InvalidContext{"Additional string", 42};
+    } catch (const net::Exception& e) {
+        std::cout << "code => " << e.code() << std::endl;
+        std::cout << "what => " << e.what() << std::endl;
+    }
+    ASSERT_TRUE(true);
+}
+
+TEST(Typecheck, enum_errc) {
+    net::Exception e1{net::AdapterErrors::InvalidContext};
+    //net::Exception e2{static_cast<int>(42)};
+    //net::Exception e3{std::string{"err"}};
+    net::Exception e4{net::AdapterErrors::InvalidContext, "42"};
+    net::Exception e5{net::AdapterErrors::InvalidContext, std::string{"42"}};
+    //net::Exception e6{net::AdapterErrors::InvalidContext, 42};
+    ASSERT_TRUE(true);
+}
